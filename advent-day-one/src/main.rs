32409aee825f7bd5ba1848aaa8b2bc247e3d3393
Clinton Medbery <clintonmedbery@users.noreply.github.com>
2024-12-05 22:43:02 +0000

    some improvements

--- advent-day-one/src/main.rs
+++ advent-day-one/src/main.rs
@@ -1,9 +1,12 @@
 use std::fs;
 use std::io;
+use std::time::Instant;
 
 //https://adventofcode.com/2024/day/1
 
 fn main() -> io::Result<()> {
+    let now = Instant::now();
+
     // Specify the relative path to the file
     let file_path = "./src/puzzle.txt";
 
@@ -11,27 +14,59 @@ fn main() -> io::Result<()> {
     let contents = fs::read_to_string(file_path)?;
     let lines: Vec<&str> = contents.lines().collect();
     // make a new vector to store the lines
-    let mut lines_left: Vec<String> = Vec::new();
-    let mut lines_right: Vec<String> = Vec::new();
+    let mut lines_left: Vec<i32> = Vec::new();
+    let mut lines_right: Vec<i32> = Vec::new();
     for line in lines {
-        let line_arr: Vec<&str> = line.split(" ").collect::<Vec<&str>>();
-        lines_left.push(line_arr[0].to_string());
-        lines_right.push(line_arr[1].to_string());
+        let line_arr: Vec<&str> = line.split_whitespace().collect::<Vec<&str>>();
+        let new_left = line_arr[0].parse::<i32>().unwrap();
+        let new_right = line_arr[1].parse::<i32>().unwrap();
+
+        let mut left_insert = false;
+        let mut right_insert = false;
+
+        if lines_left.len() == 0 {
+            lines_left.push(new_left);
+            lines_right.push(new_right);
+            left_insert = true;
+            right_insert = true;
+        }
+        for i in 0..lines_left.len() {
+            if left_insert && right_insert {
+                break;
+            }
+
+            if lines_left[i] >= new_left && !left_insert {
+                lines_left.insert(0, new_left);
+                left_insert = true;
+            }
+            if lines_right[i] >= new_right && !right_insert {
+                lines_right.insert(0, new_right);
+                right_insert = true;
+            }
+            
+        }
+        if !left_insert {
+            lines_left.push(new_left);
+        }
+        if !right_insert {
+            lines_right.push(new_right);
+        }
+
     }
+    // Hmmm shouldn't need this
     lines_left.sort();
     lines_right.sort();
+    println!("{:?}", lines_right);
     let mut value = 0;
-    for (i, left_str) in lines_left.iter().enumerate() {
-        println!("{}", left_str);
-        let left = left_str.parse::<i32>().unwrap();
-        let right_str = lines_right[i].clone();
-        println!("{}", right_str);
-        let right = right_str.parse::<i32>().unwrap();
-
+    for (i, left) in lines_left.iter().enumerate() {
+        let right = lines_right[i];
         let amount = (left - right).abs();
         value += amount;
     }
 
     println!("{}", value);
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
     Ok(())
+    // 3574690
 }
